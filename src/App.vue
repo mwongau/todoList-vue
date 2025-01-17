7218e1670e35d7ff9d303d364ddc3dd9f1d7e38c
M Wong <wongmt15@gmail.com>
2025-01-18 04:09:21 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+let id = 0
+const newItem = ref('')
+const items = ref([])
+
+function addItem() {
+  items.value.push({ id: id++, text: newItem.value })
+  newItem.value = ''
+}
+
+function removeItem(item) {
+  items.value = items.value.filter((t) => t !== item)
+}
+</script>
+
+<template>
+    <input v-model="newItem" placeholder="Enter new item"> &nbsp;
+    <button @click="addItem">Add</button>
+  
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{ item.text }}
+      <button @click="removeItem(item)">Delete</button>
+    </li>
+  </ul>
+</template>
